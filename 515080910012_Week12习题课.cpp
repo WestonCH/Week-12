9fd0339869eb5ce24d33d74b9dc1504f251b2c0c
程浩 <weastonCH@sjtu.edu.cn>
2017-05-09 16:43:49 +0800

    no message

---
+++ 515080910012_Week12习题课.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+#include <string>
+#include <queue>
+#include <unordered_set>
+#include <stack>
+#include <fstream>
+#include <sstream>
+#include <set>
+#include <map>
+#include <algorithm>
+using namespace std;
+
+struct Compare {
+	bool operator()(const pair<string,int>& lhs, const pair<string, int>& rhs) {
+		return lhs.second < rhs.second;
+	}
+};  
+
+int main()
+{
+	ifstream file;
+
+	while (true)
+	{
+		int thing;
+		
+		cout << "input '0' to input 'n','1' to input str:";
+		cin >> thing;
+
+		if (thing == 0)
+		{
+			int n;
+			string line;
+			map <string, int> output;
+			
+			while (true)
+			{
+				cout << "please input the number 'n': ";
+				cin >> n;
+				if (n > 0)
+				{
+					file.open("dictionary.txt");
+					while (getline(file, line))
+					{
+						if (line.length() >= n)
+						{
+							string str = line.substr(line.length() - n);
+							//dic.insert(str);
+							output[str] += 1;
+						}
+
+					}
+					vector <pair<string, int>> get_all(output.begin(), output.end());
+					sort(get_all.begin(), get_all.end(), Compare());
+					for (int i = get_all.size() - 1; i >= get_all.size() - 10; i--)
+					{
+						cout << get_all[i].first << "     " << get_all[i].second << endl;
+					}
+					file.close();
+					break;
+				}
+				else continue;
+			}
+		}
+
+		if (thing == 1)
+		{
+			string line;
+			string word;
+			stack<string> outside;
+
+			int count = 0;
+
+			cout << "please input the str:";	
+			cin >> word;
+
+			file.open("dictionary.txt");
+			while (getline(file, line))
+			{
+				if (line.length() >= word.length())
+				{
+					string str = line.substr(line.length() - word.length());
+					if (str == word)
+					{
+						outside.push(line);						
+					}
+				}
+			}
+			for (int i = 0; i < outside.size(); i++)
+			{
+				cout << outside.top() << endl;
+				outside.pop();
+			}
+			cout << "the number of words is: " << outside.size() << endl;
+			file.close();
+		}
+		else continue;
+	}
+	
+}
